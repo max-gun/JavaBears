281e2b5e99496943672e2969988b843ac85cc44c
Max Gun <max.gun@nielsen.com>
2024-01-29 21:08:52 +0200

    stack and queue

---
+++ src/main/java/structs/stack/OurQueue.java
@@ -0,0 +1,28 @@
+package structs.stack;
+
+public class OurQueue {
+
+    public void enqueue(String data) {
+
+    }
+
+    public String dequeue() {
+
+    }
+
+    public String peek() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void print() {
+
+    }
+}
---
+++ src/main/java/structs/stack/OurStack.java
@@ -0,0 +1,41 @@
+package structs.stack;
+
+import structs.linkedlist.LinkedList;
+import structs.linkedlist.LinkedListNode;
+
+import static java.util.Objects.nonNull;
+
+public class OurStack {
+
+    private LinkedList list;
+    private static final int INSERTION_INDEX = 0;
+
+    public OurStack() {
+        list = new LinkedList();
+    }
+
+    public void push(String data) {
+        list.add(data, INSERTION_INDEX);
+    }
+
+    public String pop() {
+        LinkedListNode node = list.pull(INSERTION_INDEX);
+        return nonNull(node) ? node.getData() : "";
+    }
+
+    public String peek() {
+        return list.get(INSERTION_INDEX);
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public void print() {
+        System.out.println(list);
+    }
+}
