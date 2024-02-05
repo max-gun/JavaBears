a8424df57007cf7d9fefb690325bdd3eca9ec26c
max-gun <22318798+max-gun@users.noreply.github.com>
2024-02-05 18:05:25 +0200

    numbers in array interview question

--- src/main/java/misc/Euler.java
+++ src/main/java/misc/Euler.java
@@ -1,11 +1,32 @@
 package misc;
 
+import java.util.stream.IntStream;
+
 public class Euler {
 
 
     public static void main(String[] args) {
-        long result = firstEulerSolution(1_000_000_000_000L);
-        System.out.println(result);
+        //long result = firstEulerSolution(1_000_000_000_000L);
+        //System.out.println(result);
+
+        int[] arr = {1,8,3,4,2,10,9,7,6};
+        findMissingNumber(arr);
+    }
+
+    public static void findTwoMissingNumbers(int[] numbers) {
+
+    }
+
+    private static void findMissingNumber(int[] numbers) {
+        int inputSum = 0;
+        int naturalSum = numbers.length + 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            inputSum += numbers[i];
+            naturalSum += (i+1);
+        }
+
+        System.out.println(naturalSum - inputSum);
     }
 
     private static long firstEulerSolution(long num) {
@@ -18,4 +39,5 @@ public class Euler {
 
         return sum;
     }
+
 }
