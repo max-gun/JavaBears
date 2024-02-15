3a400638649f55e06d898697bdaf9a7f8eecd9d4
max-gun <22318798+max-gun@users.noreply.github.com>
2024-02-15 19:48:50 +0200

    boot start

--- src/main/java/springs/bootcamp/controller/StudentsController.java
+++ src/main/java/springs/bootcamp/controller/StudentsController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class StudentsController {
 
-    @RequestMapping("/hello")
-    public String sayHi(){
-        return "Hi";
+    @RequestMapping("/countStudents")
+    public Integer countStudents() {
+        System.out.println("Starting count students");
+        return 25;
     }
 }
